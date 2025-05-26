87b1c60df7cdbb86b8e325f21636c76473db8cd1
manrique <1601836@senati.pe>
2025-05-26 17:18:40 -0500

    web

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/index.css">
+</head>
+<body>
+    <header>
+        <div class="menu">
+            <div class="super_menu">
+                <h1>Marca de de venta</h1>
+            </div>
+            <div class="min_menu">
+                <nav>
+                    <ul>
+                        <li><a href="#one">secion one</a></li>
+                        <li><a href="#two">secion two</a></li>
+                        <li><a href="#three">secion three</a></li>
+                        <li><a href="#four">secion four</a></li>
+                        <li><a href="#five">secion five</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="home">
+            <div class="index">
+                <h2>Welcome in your home future</h2>
+            </div>
+        </div>
+        <section id="one">
+            <img src="img/charizard.png" alt="Charizard Pokemon">
+        </section>
+        <section id="two">
+            <h3>
+                secion two
+            </h3>
+        </section>
+        <section id="three">
+            <h3>
+                secion three
+            </h3>
+        </section>
+        <section id="four">
+            <h3>
+                secion four
+            </h3>
+        </section>
+        <section id="five">
+            <h3>
+                secion five
+            </h3>
+        </section>
+    </main>
+    <footer>
+        <div class="sub_footer">
+            <ul>
+                <li><a href="">hoja</a></li>
+                <li><a href="">hoja</a></li>
+                <li><a href="">hoja</a></li>
+                <li><a href="">hoja</a></li>
+                <li><a href="">hoja</a></li>
+                <li><a href="">hoja</a></li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
